16df935465a90aff5c58f61ed67830592d3e1280
DDDisch <philipp.dvorak@sz-ybbs.ac.at>
2020-12-21 09:04:18 +0100

    Fixed Animation and Event Infinite Loading

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -155,6 +155,7 @@ export default {
     },mounted() {
         this.$nextTick(() => {
             this.$nuxt.$loading.start();
+            setTimeout(() => this.$nuxt.$loading.finish(), 500)
         })
     },
     created() {
