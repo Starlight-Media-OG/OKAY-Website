d58dc7da77c8b4215da7d0fe9ebdcfdc7c326889
root <root@server.okay-ybbs.at>
2021-02-27 11:09:46 +0100

    Sync with Local

--- nuxt-dist/router.js
+++ nuxt-dist/router.js
@@ -3,20 +3,20 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _3a33a6e1 = () => interopDefault(import('../pages/calender/index.vue' /* webpackChunkName: "pages/calender/index" */))
-const _367cdb40 = () => interopDefault(import('../pages/dsgvo/index.vue' /* webpackChunkName: "pages/dsgvo/index" */))
-const _f9cf5700 = () => interopDefault(import('../pages/events/index.vue' /* webpackChunkName: "pages/events/index" */))
-const _defb8874 = () => interopDefault(import('../pages/impressum/index.vue' /* webpackChunkName: "pages/impressum/index" */))
-const _6aed553b = () => interopDefault(import('../pages/information/index.vue' /* webpackChunkName: "pages/information/index" */))
-const _777710ba = () => interopDefault(import('../pages/news/index.vue' /* webpackChunkName: "pages/news/index" */))
-const _2298f00b = () => interopDefault(import('../pages/products/index.vue' /* webpackChunkName: "pages/products/index" */))
-const _0c9d1230 = () => interopDefault(import('../pages/events/_id.vue' /* webpackChunkName: "pages/events/_id" */))
-const _c58f6d72 = () => interopDefault(import('../pages/images/_id.vue' /* webpackChunkName: "pages/images/_id" */))
-const _0db40cbc = () => interopDefault(import('../pages/news/_id.vue' /* webpackChunkName: "pages/news/_id" */))
-const _17297933 = () => interopDefault(import('../pages/products/_id.vue' /* webpackChunkName: "pages/products/_id" */))
-const _33214a49 = () => interopDefault(import('../pages/projects/_id.vue' /* webpackChunkName: "pages/projects/_id" */))
-const _5a63d738 = () => interopDefault(import('../pages/unsubscribe/_mail.vue' /* webpackChunkName: "pages/unsubscribe/_mail" */))
-const _5283f05e = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _a1c5a470 = () => interopDefault(import('../pages/calender/index.vue' /* webpackChunkName: "pages/calender/index" */))
+const _044815b9 = () => interopDefault(import('../pages/dsgvo/index.vue' /* webpackChunkName: "pages/dsgvo/index" */))
+const _6eb46927 = () => interopDefault(import('../pages/events/index.vue' /* webpackChunkName: "pages/events/index" */))
+const _8e6cdc82 = () => interopDefault(import('../pages/impressum/index.vue' /* webpackChunkName: "pages/impressum/index" */))
+const _c291de18 = () => interopDefault(import('../pages/information/index.vue' /* webpackChunkName: "pages/information/index" */))
+const _4c8e2321 = () => interopDefault(import('../pages/news/index.vue' /* webpackChunkName: "pages/news/index" */))
+const _d0fb121c = () => interopDefault(import('../pages/products/index.vue' /* webpackChunkName: "pages/products/index" */))
+const _626eed62 = () => interopDefault(import('../pages/events/_id.vue' /* webpackChunkName: "pages/events/_id" */))
+const _724f5bae = () => interopDefault(import('../pages/images/_id.vue' /* webpackChunkName: "pages/images/_id" */))
+const _21dc79c9 = () => interopDefault(import('../pages/news/_id.vue' /* webpackChunkName: "pages/news/_id" */))
+const _02c58dda = () => interopDefault(import('../pages/products/_id.vue' /* webpackChunkName: "pages/products/_id" */))
+const _1ebd5ef0 = () => interopDefault(import('../pages/projects/_id.vue' /* webpackChunkName: "pages/projects/_id" */))
+const _0697d01d = () => interopDefault(import('../pages/unsubscribe/_mail.vue' /* webpackChunkName: "pages/unsubscribe/_mail" */))
+const _83f28cd2 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -36,59 +36,59 @@ export const routerOptions = {
 
   routes: [{
     path: "/calender",
-    component: _3a33a6e1,
+    component: _a1c5a470,
     name: "calender"
   }, {
     path: "/dsgvo",
-    component: _367cdb40,
+    component: _044815b9,
     name: "dsgvo"
   }, {
     path: "/events",
-    component: _f9cf5700,
+    component: _6eb46927,
     name: "events"
   }, {
     path: "/impressum",
-    component: _defb8874,
+    component: _8e6cdc82,
     name: "impressum"
   }, {
     path: "/information",
-    component: _6aed553b,
+    component: _c291de18,
     name: "information"
   }, {
     path: "/news",
-    component: _777710ba,
+    component: _4c8e2321,
     name: "news"
   }, {
     path: "/products",
-    component: _2298f00b,
+    component: _d0fb121c,
     name: "products"
   }, {
     path: "/events/:id",
-    component: _0c9d1230,
+    component: _626eed62,
     name: "events-id"
   }, {
     path: "/images/:id?",
-    component: _c58f6d72,
+    component: _724f5bae,
     name: "images-id"
   }, {
     path: "/news/:id",
-    component: _0db40cbc,
+    component: _21dc79c9,
     name: "news-id"
   }, {
     path: "/products/:id",
-    component: _17297933,
+    component: _02c58dda,
     name: "products-id"
   }, {
     path: "/projects/:id?",
-    component: _33214a49,
+    component: _1ebd5ef0,
     name: "projects-id"
   }, {
     path: "/unsubscribe/:mail?",
-    component: _5a63d738,
+    component: _0697d01d,
     name: "unsubscribe-mail"
   }, {
     path: "/",
-    component: _5283f05e,
+    component: _83f28cd2,
     name: "index"
   }],
 
